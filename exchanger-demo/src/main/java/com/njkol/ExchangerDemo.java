b5cbb363ac3dd1ceb3e94531dfd19459a6848929
Nj-Kol <nilanjan.sarkar100@gmail.com>
2022-01-22 20:31:35 +0530

    first commit

---
+++ exchanger-demo/src/main/java/com/njkol/ExchangerDemo.java
@@ -0,0 +1,31 @@
+package com.njkol;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Exchanger;
+
+public class ExchangerDemo {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		Exchanger<List<String>> exchanger = new Exchanger<List<String>>();
+		List<String> sample = new ArrayList<String>();
+		Producer p = new Producer(exchanger,sample);
+		Consumer c = new Consumer(exchanger,sample);
+		
+		Thread pt = new Thread(p,"Producer");
+		Thread ct = new Thread(c,"Consumer");
+		
+		pt.start();
+		ct.start();
+		
+		Thread.sleep(500L);
+		
+		p.setActive(false);
+		c.setActive(false);
+		
+        System.exit(0);
+
+	}
+
+}
---
+++ java-concurrency-demos/src/test/java/com/njkol/TestThreadLocal.java
@@ -0,0 +1,41 @@
+package com.njkol;
+
+import org.junit.jupiter.api.Test;
+
+import com.njkol.threadlocal.MyRunnable;
+import com.njkol.threadlocal.Request;
+
+public class TestThreadLocal {
+
+	//@Test
+	public void testSimple() throws InterruptedException {
+		
+		MyRunnable sharedRunnableInstance = new MyRunnable();
+
+		Thread thread1 = new Thread(sharedRunnableInstance);
+		Thread thread2 = new Thread(sharedRunnableInstance);
+
+		thread1.start();
+		thread2.start();
+
+		thread1.join(); // wait for thread 1 to terminate
+		thread2.join(); // wait for thread 2 to terminate
+	}
+	
+	@Test
+	public void testServerDemo() throws InterruptedException {
+		
+		Request req = new Request();
+		
+		Thread thread1 = new Thread(req);
+		Thread thread2 = new Thread(req);
+		
+		thread1.start();
+		thread2.start();
+
+		thread1.join(); // wait for thread 1 to terminate
+		thread2.join(); // wait for thread 2 to terminate
+				
+	}
+	
+}
